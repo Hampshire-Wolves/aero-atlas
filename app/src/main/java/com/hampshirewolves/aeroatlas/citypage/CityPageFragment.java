150c3810c4132828855a029649e58dd67801dcfc
Vinay Patel <vinayspatel0@gmail.com>
2025-01-13 15:32:55 +0000

    refactor: moved CityPageFragment java file to citypage directory add import com.hampshirewolves.aeroatlas.R;

--- app/src/main/java/com/hampshirewolves/aeroatlas/citypage/CityPageFragment.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/citypage/CityPageFragment.java
@@ -1,4 +1,4 @@
-package com.hampshirewolves.aeroatlas;
+package com.hampshirewolves.aeroatlas.citypage;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hampshirewolves.aeroatlas.R;
 
 /**
  * A simple {@link Fragment} subclass.
