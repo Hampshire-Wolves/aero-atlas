b10d5cc0a6b6fdc5d85e3421db7588acfd1d0071
Valdis <v.beikmanis@gmail.com>
2025-01-13 22:47:49 +0000

    feat: 31-profile-unregistered-fragment (UI)

--- app/src/main/java/com/hampshirewolves/aeroatlas/MainActivity.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/MainActivity.java
@@ -1,44 +1,24 @@
 package com.hampshirewolves.aeroatlas;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.navigation.NavigationBarView;
-
-public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
-
-    NavigationBarView bottomNavBar;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        bottomNavBar = findViewById(R.id.bottomnavbar);
-
-        bottomNavBar.setOnItemSelectedListener(this);
-
-        bottomNavBar.setSelectedItemId(R.id.homebutton);
-
-    }
-
-//    Declare each fragment here i.e homepage, city view, etc
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        if (item.getItemId() == R.id.homebutton)
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.baseFragment, FRAGMENT_NAME)
-                    .commit();
-        return false;
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
