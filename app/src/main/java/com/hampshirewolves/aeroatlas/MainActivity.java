b43ee25f6a4efe9c4275ed8bd7869832aac3f147
WI91 <elammar.will@gmail.com>
2025-01-14 10:24:47 +0000

    fix: fix conflict

--- app/src/main/java/com/hampshirewolves/aeroatlas/MainActivity.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/MainActivity.java
@@ -1,24 +1,46 @@
 package com.hampshirewolves.aeroatlas;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+import com.hampshirewolves.aeroatlas.homepage.HomepageFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    NavigationBarView bottomNavBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bottomNavBar = findViewById(R.id.bottomnavbar);
+
+        bottomNavBar.setOnItemSelectedListener(this);
+
+        bottomNavBar.setSelectedItemId(R.id.homebutton);
+
+    }
+
+//   TODO  Declare each fragment here i.e homepage, city view, etc
+HomepageFragment homepageFragment = new HomepageFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.homebutton)
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.baseFragment, homepageFragment)
+                    .commit();
+
+        return true;
     }
 }
