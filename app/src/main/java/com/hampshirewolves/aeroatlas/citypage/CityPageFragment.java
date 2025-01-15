4231da9aa37a8e3b9593143ef85f7b1126ba90cd
Vinay Patel <vinayspatel0@gmail.com>
2025-01-15 09:38:22 +0000

    refactor: remove boiler plate add variables cityAttractionsList, cityAttractionsRecyclerView, cityAttractionsAdapter

--- app/src/main/java/com/hampshirewolves/aeroatlas/citypage/CityPageFragment.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/citypage/CityPageFragment.java
@@ -3,58 +3,26 @@ package com.hampshirewolves.aeroatlas.citypage;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.hampshirewolves.aeroatlas.R;
+import com.hampshirewolves.aeroatlas.model.City;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CityPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class CityPageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<CityAttractions> cityAttractionsList;
+    private RecyclerView cityAttractionsRecyclerView;
+    private CityAttractionsAdapter cityAttractionsAdapter;
 
     public CityPageFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CityPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CityPageFragment newInstance(String param1, String param2) {
-        CityPageFragment fragment = new CityPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,4 +31,7 @@ public class CityPageFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_city_page, container, false);
     }
+
+
+
 }
