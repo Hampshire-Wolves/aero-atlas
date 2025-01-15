e8b43f0f3b9b0ffaabdc913e47bc1e4abcf6a20f
kofoworola <kay16th@yahoo.com>
2025-01-15 15:48:58 +0000

    feat: Implement functionality for SignupActivity page
    - Add Firebase authentication logic for user account creation.
    - Include input validation for email, password and confirm password fields.
    - Display error messages for invalid or empty input fields.
    - Integrate progress bar visibility during signup attempts.
    - Redirect users to the main page upon successful account creation.
    - Enable redirection to the login page via a button click.

--- app/src/main/java/com/hampshirewolves/aeroatlas/SignupActivity.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/SignupActivity.java
@@ -1,24 +1,119 @@
 package com.hampshirewolves.aeroatlas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hampshirewolves.aeroatlas.Handlers.UIHandler;
 
 public class SignupActivity extends AppCompatActivity {
+    EditText signupPageEmailInputBox, signupPagePasswordInputBox, signupPageConfirmPasswordInputBox;
+    ImageView signupPageBackButton;
+    Button signupPageSignupButton, signupPageRedirectToLoginButton;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signupPageBackButton = findViewById(R.id.signupPageBackButton);
+        signupPageEmailInputBox = findViewById(R.id.signupPageEmailInputBox);
+        signupPagePasswordInputBox = findViewById(R.id.signupPagePasswordInputBox);
+        signupPageConfirmPasswordInputBox = findViewById(R.id.signupPageConfirmPasswordInputBox);
+        signupPageSignupButton = findViewById(R.id.signupPageSignupButton);
+        signupPageRedirectToLoginButton = findViewById(R.id.signupPageRedirectToLoginButton);
+        progressBar = findViewById(R.id.signupProgressBar);
+
+        signupPageBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signupPageRedirectToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signupPageSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                String email = signupPageEmailInputBox.getText().toString();
+                String password = String.valueOf(signupPagePasswordInputBox.getText());
+                String confirmPassword = String.valueOf(signupPageConfirmPasswordInputBox.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    UIHandler.handleValidationError(progressBar, SignupActivity.this, "Email cannot be empty");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                    UIHandler.handleValidationError(progressBar, SignupActivity.this, "Password cannot be empty");
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    UIHandler.handleValidationError(progressBar, SignupActivity.this, "Passwords do not match");
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Account created", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         });
     }
 }
