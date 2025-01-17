98dd7cbd5a3ade7ce0f4740f1533f1e16a5576cf
kofoworola <kay16th@yahoo.com>
2025-01-17 12:34:09 +0000

    feat: Implement back button functionality on the Login/Signup page to navigate back to DiscoveryFragment.
    Add logs to Login and Signup pages for debugging

--- app/src/main/java/com/hampshirewolves/aeroatlas/SignupActivity.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/SignupActivity.java
@@ -1,8 +1,11 @@
 package com.hampshirewolves.aeroatlas;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -59,7 +62,8 @@ public class SignupActivity extends AppCompatActivity {
         signupPageBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("navigateTo", "DiscoverPageFragment");
                 startActivity(intent);
                 finish();
             }
@@ -103,6 +107,7 @@ public class SignupActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressBar.setVisibility(View.GONE);
                         if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
                             Toast.makeText(SignupActivity.this, "Account created", Toast.LENGTH_SHORT).show();
 
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
