d72891a73a0236090a06c3342635754e9170c2e9
WI91 <elammar.will@gmail.com>
2025-01-17 09:49:07 +0000

    feat: add flight adaptor class

--- app/src/main/java/com/hampshirewolves/aeroatlas/ui/mainactivity/FlightAdapter.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/ui/mainactivity/FlightAdapter.java
@@ -1,4 +1,20 @@
 package com.hampshirewolves.aeroatlas.ui.mainactivity;
 
-public class FlightAdapter {
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hampshirewolves.aeroatlas.homepage.RecyclerViewInterface;
+import com.hampshirewolves.aeroatlas.model.Flights;
+
+import java.util.List;
+
+public class FlightAdapter extends RecyclerView.Adapter<> {
+
+    private List<Flights> flightsList;
+    private RecyclerViewInterface recyclerViewInterface;
+
+
+    public static class FlightViewHolder extends RecyclerView.ViewHolder {
+
+
+    }
 }
