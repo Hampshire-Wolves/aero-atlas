54c5ec295c9c5f8a469f172364292f0807705f6c
WI91 <elammar.will@gmail.com>
2025-01-20 12:46:31 +0000

    feat: refactored flight adaptor

--- app/src/main/java/com/hampshirewolves/aeroatlas/model/Flights.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/model/Flights.java
@@ -1,6 +1,30 @@
 package com.hampshirewolves.aeroatlas.model;
 
-public class Flights {
+import android.os.Parcelable;
 
-    //TODO Create flight model - Objects within class need own classes with parcelable implementation
+import androidx.databinding.BaseObservable;
+
+public class Flights extends BaseObservable {
+
+    private String id;
+    private String price;
+    private String currency;
+    private String origin;
+    private String destination;
+    private String departureDate;
+    private String arrivalDate;
+
+
+    public Flights() {
+    }
+
+    public Flights(String id, String price, String currency, String origin, String destination, String departureDate, String arrivalDate) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+    }
 }
--- app/src/main/java/com/hampshirewolves/aeroatlas/ui/mainactivity/FlightAdapter.java
+++ app/src/main/java/com/hampshirewolves/aeroatlas/ui/mainactivity/FlightAdapter.java
@@ -1,7 +1,6 @@
 package com.hampshirewolves.aeroatlas.ui.mainactivity;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
